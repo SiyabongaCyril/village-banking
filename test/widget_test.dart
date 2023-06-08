c42545e43bc118dba63c6de735986e724a66e3d8
Siyabonga Ntshingila <siya3.cyril@gmail.com>
2023-06-08 22:11:17 +0200

    Welcome and Signin

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:villagebanking/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const VillageApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
